4c18831a729af46d8e6fe08cc42a720340ec457d
manas <tmanas2004@gmail.com>
2024-07-15 17:27:37 +0530

    abc

--- src/routes/profiles/+page.server.ts
+++ src/routes/profiles/+page.server.ts
@@ -1,110 +1,139 @@
-import { createPool, sql } from '@vercel/postgres';
-import { POSTGRES_URL } from '$env/static/private';
+// import { error } from '@sveltejs/kit';
+import { createPool, sql } from '@vercel/postgres'
+import { POSTGRES_URL } from '$env/static/private'
 
 export async function load() {
-  const db = createPool({ connectionString: POSTGRES_URL });
+  const db = createPool({ connectionString: POSTGRES_URL })
 
   try {
-    const { rows: names } = await db.query('SELECT * FROM names');
-    return { names };
+    const { rows: names } = await db.query('SELECT * FROM names order by id')
+    return {
+      names: names,
+    }
   } catch (error) {
-    console.log('Table does not exist, creating and seeding it with dummy data now...');
-    await seed();
-    const { rows: names } = await db.query('SELECT * FROM names');
-    return { names };
-  }
+      console.log(
+        'Table does not exist, creating and seeding it with dummy data now...'
+      )
+      // Table is not created yet
+      await seed()
+      const { rows: names } = await db.query('SELECT * FROM names order by id')
+      return {
+        users: names
+      }
+    } 
 }
 
 async function seed() {
-  const db = createPool({ connectionString: POSTGRES_URL });
+  const db = createPool({ connectionString: POSTGRES_URL })
   const client = await db.connect();
-  await client.sql`
-    CREATE TABLE IF NOT EXISTS names (
+  const createTable = await client.sql`CREATE TABLE IF NOT EXISTS names (
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
       "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
-  `;
-  console.log('Created "names" table');
+    `
+
+  console.log('Created "users" table')
 
   const users = await Promise.all([
     client.sql`
-      INSERT INTO names (name, email)
-      VALUES ('Rohan', 'rohan@tcl.com')
-      ON CONFLICT (email) DO NOTHING;
-    `,
+          INSERT INTO names (name, email)
+          VALUES ('Rohan', 'rohan@tcl.com')
+          ON CONFLICT (email) DO NOTHING;
+      `,
     client.sql`
-      INSERT INTO names (name, email)
-      VALUES ('anzzuel', 'anzzzzuel@gmail.com')
-      ON CONFLICT (email) DO NOTHING;
-    `,
+          INSERT INTO names (name, email, image)
+          VALUES ('Rebecca', 'rebecca@tcl.com')
+          ON CONFLICT (email) DO NOTHING;
+      `,
     client.sql`
-      INSERT INTO names (name, email)
-      VALUES ('Rebecca', 'rebecca@tcl.com')
-      ON CONFLICT (email) DO NOTHING;
-    `,
-    client.sql`
-      INSERT INTO names (name, email)
-      VALUES ('Vivek', 'vivek@gmail.com')
-      ON CONFLICT (email) DO NOTHING;
-    `,
-  ]);
-  console.log(`Seeded ${users.length} users`);
-  client.release();
+          INSERT INTO names (name, email, image)
+          VALUES ('Vivek', 'vivek@gmail.com')
+          ON CONFLICT (email) DO NOTHING;
+      `,
+  ])
+  console.log('Seeded ${users.length} users')
 
-  return { users };
+  return {
+    createTable,
+    users,
+  }
+}
+
+async function updateUser(user: { id: any; name: any; email: any }) {
+    console.log('user', user);
+    const db = createPool({ connectionString: POSTGRES_URL })
+    const client = await db.connect();
+
+    const result = await client.sql`UPDATE names SET name = ${user.name}, email = ${user.email} WHERE id = ${user.id}`
+
+    return {
+      result
+    }
 }
 
 /** @type {import('./$types').Actions} */
 export const actions = {
+	
   update: async ({ request }: { request: any }) => {
-    const data = await request.formData();
-    const db = createPool({ connectionString: POSTGRES_URL });
-    const client = await db.connect();
+    const req = await request.formData();
 
-    const id = data.get('id');
-    const email = data.get('email');
-    const name = data.get('name');
+    const id = req.get('id');
+    const name = req.get('name');
+    const email = req.get('email');
 
-    await client.sql`
-      UPDATE names
-      SET email = ${email}, name = ${name}
-      WHERE id = ${id};
-    `;
-    client.release();
-    return { success: true };
-  },
+    const data = {
+      id, name, email
+    }
+
+    let updateRes = {
+      error : false, email : email, name, messsage : ''
+    }
+
+    try {
+      const res = await updateUser(data);
+      console.log('api request ran');
+      console.log(res);
+
+
+    } catch (error) {
+        console.log('api request errored');
+        console.log(error)
+        updateRes.error = true;
+        updateRes.messsage = (error as Error).message;
+    }finally{
+      return updateRes
+    }
+	},
 
   delete: async ({ request }: { request: any }) => {
     const data = await request.formData();
-    const db = createPool({ connectionString: POSTGRES_URL });
+    const db = createPool({ connectionString: POSTGRES_URL })
     const client = await db.connect();
 
-    const id = data.get('id');
+    const id = String(data.get('id'));
 
-    await client.sql`
-      DELETE FROM names
-      WHERE id = ${id};
-    `;
-    client.release();
-    return { success: true };
-  },
+    const deleteUser = await client.sql`
+    DELETE FROM names
+    WHERE id = ${id};`
+	
+		return { deleted: true };
+	},
 
-  create: async ({ request }: { request: any }) => {
-    const data = await request.formData();
-    const db = createPool({ connectionString: POSTGRES_URL });
+  create: async ({request}: { request: any }) => {
+		const data = await request.formData();
+    const db = createPool({ connectionString: POSTGRES_URL })
     const client = await db.connect();
 
     const email = data.get('email');
-    const name = data.get('name');
+		const name = data.get('name');
 
-    await client.sql`
-      INSERT INTO names (name, email)
-      VALUES (${name}, ${email})
-      ON CONFLICT (email) DO NOTHING;
-    `;
-    client.release();
+    const createUser = await client.sql`
+          INSERT INTO names (name, email)
+          VALUES (${String(name)}, ${String(email)})
+          ON CONFLICT (email) DO NOTHING;
+        `
     return { success: true };
-  }
+	}
 };
